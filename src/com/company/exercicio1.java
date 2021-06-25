22aed8acbebbce444bfce64ab8cba937cb60bb8f
Angelo Salles <angelo.salles@mercadolivre.com>
2021-06-25 14:04:46 -0300

    exercicio 1 aula 3

---
+++ src/com/company/exercicio1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+    public static void main(String[] args) {
+        // Exercicio 1
+	    Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um número");
+        int n = ler.nextInt();
+        for (int i = 0; i <= n; i++) {
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/com/company/exercicio3.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class exercicio3 {
+
+    public static boolean ehPrimo(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Exercicio 3
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um numero");
+        int n = ler.nextInt();
+
+
+        if (ehPrimo(n)) {
+            System.out.printf("O número %d é primo", n);
+        } else {
+            System.out.printf("O número %d não é primo", n);
+        }
+    }
+
+}
