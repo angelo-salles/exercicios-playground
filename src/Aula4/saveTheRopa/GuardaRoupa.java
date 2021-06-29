f7e83bd2fc9bd6fabfd27f352b6508bfaee6b8e3
Angelo Salles <angelo.salles@mercadolivre.com>
2021-06-29 19:11:26 -0300

    programa com metodo main

--- src/Aula4/saveTheRopa/GuardaRoupa.java
+++ src/Aula4/saveTheRopa/GuardaRoupa.java
@@ -23,18 +23,4 @@ public class GuardaRoupa {
         List<Vestuario> roupas = this.dicionario.get(id);
         return roupas;
     }
-
-    public static void main(String[] args) {
-        ArrayList<Vestuario> objects = new ArrayList<Vestuario>();
-        objects.add(new Vestuario("Zara", "Camisa"));
-        objects.add(new Vestuario("Outfit", "Muito caro"));
-        objects.add(new Vestuario("Adidas", "Tenis"));
-        objects.add(new Vestuario("Nike", "Chuteira"));
-        objects.add(new Vestuario("Polo", "Camisa"));
-        GuardaRoupa guardaRoupa = new GuardaRoupa();
-        int a = guardaRoupa.guardarVestuarios(objects);
-        guardaRoupa.mostrarVestuarios();
-        List<Vestuario> b = guardaRoupa.devolverVestuarios(a);
-        System.out.println(b);
-    }
 }
